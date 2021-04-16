76d0e05879b40170e73baba4212e40f4fe9dae22
JSpickelmier <78034029+JSpickelmier@users.noreply.github.com>
2021-04-16 15:00:05 -0500

    8th commit Final Project

---
+++ CIS240finalproject/src/main/java/com/mycompany/cis240finalproject/Sattelites.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.cis240finalproject;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Jacks
+ */
+public class Sattelites extends Planet {
+    private Planet HomePlanet;
+    
+    public Sattelites (String Name, double Traveltime, double DeltaVCapture, double DeltaVLowOrbit, double DeltaVLanding, ArrayList<Sattelites> Moons) {
+        super(Name, Traveltime, DeltaVCapture, DeltaVLowOrbit, DeltaVLanding, Moons);
+        this.HomePlanet = HomePlanet;
+        
+    }
+
+    public Planet getHomePlanet() {
+        return HomePlanet;
+    }
+
+    public void setHomePlanet(Planet HomePlanet) {
+        this.HomePlanet = HomePlanet;
+    }
+    
+    
+}
